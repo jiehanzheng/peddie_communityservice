e7a6f51dfc5f4f218b0bcd82158b86a41816a6d7
Jiehan Zheng <zheng@jiehan.org>
2013-01-16 19:04:07 -0500

    Unicorn config and database config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "set your application name here"
+set :application, "peddie_communityservice"
 
 set :scm, :git
 set :repository,  "git@us1.jiehan.org:/srv/git/peddie_communityservice.git"
@@ -6,18 +6,21 @@ set :branch, "master"
 set :deploy_to, "/srv/rails/peddie_communityservice"
 set :deploy_via, :remote_cache
 ssh_options[:forward_agent] = true
+set :use_sudo, false
 
 role :web, "us5.jiehan.org"
 role :app, "us5.jiehan.org"
 role :db,  "us5.jiehan.org", :primary => true
 set :user, "rails"
 
+set :rvm_type, :system
 set :rvm_ruby_string, '1.9.3'
 
 
 require "rvm/capistrano"
 require "bundler/capistrano"
 
+after "deploy", "deploy:migrate"
 
-
+set :keep_releases, 3
 after "deploy:restart", "deploy:cleanup"
