be0fbc6496d56cbbb2d00c3097ba7e7c44a22872
Jiehan Zheng <zheng@jiehan.org>
2013-01-18 14:10:27 -0500

    new auth rule, tweaking Unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -3,7 +3,7 @@ shared_path = '/srv/rails/peddie_communityservice/shared'
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 4
+worker_processes 8
 
 user 'rails', 'rails'
 
@@ -20,7 +20,7 @@ working_directory current_path # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/peddie_communityservice.sock", :backlog => 64
+listen "/tmp/peddie_communityservice.sock", :backlog => 1024
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
